6407ab26bd38cb5ac21f000e959ff5e5a2a9e91f
nithinreddynarala <nithinreddynarala@gmail.com>
2025-11-21 18:50:11 +0530

    logger updated

--- logger/__init__.py
+++ logger/__init__.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 # logger/__init__.py
->>>>>>> fec7536a17c28757f785d798e7fed0291bccea99
 from .custom_logger import CustomLogger
 # Create a single shared logger instance
 GLOBAL_LOGGER = CustomLogger().get_logger("doc_portal")
